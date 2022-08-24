beb07d605016f4af1ce2ba29c383cf11c54a60f3
divine <divineobi07@gmail.com>
2022-08-24 16:46:24 +0100

    add Loading component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+import Loading from "./components/Loading/Loading";
 
 const App = () => {
   return (
     <>
       <button className="regenerate">Regenerate data</button>
       <Layout />
+      {/* <Loading /> */}
     </>
   );
 };
