d88cfe5d484e2aad77f11a781d022692125466b3
divine <divineobi07@gmail.com>
2022-08-26 20:17:48 +0100

    add form functionality

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,55 +1,26 @@
-import { useRef } from "react";
+import { useContext, useState } from "react";
+import Store from "../../store/store-context";
 import "./Form.css";
+import FormDetail from "./FormDetail";
 
 const Form = () => {
-  const minRef = useRef();
-  const maxRef = useRef();
-  const submitHandler = () => {
-    const minVal = minRef.current.value;
-    const maxVal = maxRef.current.value;
+  const { error, loading } = useContext(Store);
+  const [active, setActive] = useState(false); 
 
-    if (minVal <= -10000 || minVal >= 10000) {
-    }
+  const toggleHandler = () => {
+    setActive((prevState) => !prevState);
+  };
+
+  const deactivate = () => {
+    setActive(false);
   };
 
   return (
     <>
-      <div className="nav-pointer"></div>
-      <div className="form">
-        <div className="form-main">
-          <h1 className="form-header">Input min and max number </h1>
-          <p className="form-nb">
-            minimum number from -10,000 to max-number of 10,000
-          </p>
-          <div className="form-detail">
-            <div>
-              <label htmlFor="min">Min:</label>
-              <input
-                type="number"
-                id="min"
-                placeholder="Type a number"
-                ref={minRef}
-                onInput={(e) => (e.target.value = e.target.value.slice(0, 5))}
-              />
-            </div>
-            <div>
-              <label htmlFor="min">Max:</label>
-              <input
-                type="number"
-                id="max"
-                maxLength="5"
-                placeholder="Type a number"
-                ref={maxRef}
-                onInput={(e) => (e.target.value = e.target.value.slice(0, 5))}
-              />
-              <div className="error">dmddkdkdkkd</div>
-            </div>
-          </div>
-          <button type="submit" className="submit-btn" onClick={submitHandler}>
-            Submit
-          </button>
-        </div>
-      </div>
+      {!loading && !error && (
+        <div className="nav-pointer" onClick={toggleHandler}></div>
+      )}
+      {active && <FormDetail deactivate={deactivate} />}
     </>
   );
 };
